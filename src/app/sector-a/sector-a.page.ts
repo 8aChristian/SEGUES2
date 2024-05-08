e14a77698bf5fed2f54d25881277231a98e18cc3
MJMMiller <mateomiller1605@gmail.com>
2024-05-07 20:25:41 -0500

    90% commit

--- src/app/sector-a/sector-a.page.ts
+++ src/app/sector-a/sector-a.page.ts
@@ -7,36 +7,46 @@ import { Database, object, ref } from '@angular/fire/database';
   styleUrls: ['./sector-a.page.scss'],
 })
 export class SectorAPage implements OnInit {
-  // State arrays for different frames
+
   states: boolean[] = new Array(16).fill(false);
   states2: boolean[] = new Array(12).fill(false);
+
+  trueCount: number = 0;
+  falseCount: number = 0;
 
   constructor(private database: Database) {}
 
   ngOnInit(): void {
-    this.subscribeToDatabaseChanges(); 
+    this.subscribeToDatabaseChanges();
     this.subscribeToF3DatabaseChanges();
   }
 
   private subscribeToDatabaseChanges(): void {
-
     for (let i = 1; i <= 16; i++) {
-      const route = `/sectorA/${i <= 8 ? 'f1' : 'f2'}/c${i % 8 === 0 ? 8 : i % 8}`;
+      const frame = i <= 8 ? 'f1' : 'f2';
+      const col = i % 8 === 0 ? 8 : i % 8;
+      const route = `/sectorA/${frame}/c${col}`;
       const sectorRef = ref(this.database, route);
       object(sectorRef).subscribe(value => {
-        this.states[i - 1] = value.snapshot.val(); 
+        this.states[i - 1] = value.snapshot.val();
+        this.updateCounts();
       });
     }
   }
 
   private subscribeToF3DatabaseChanges(): void {
-
     for (let i = 1; i <= 12; i++) {
       const route = `/sectorA/f3/c${i}`;
       const sectorRef = ref(this.database, route);
       object(sectorRef).subscribe(value => {
-        this.states2[i - 1] = value.snapshot.val(); 
+        this.states2[i - 1] = value.snapshot.val();
+        this.updateCounts();
       });
     }
+  }
+
+  private updateCounts(): void {
+    this.trueCount = [...this.states, ...this.states2].filter(val => val).length;
+    this.falseCount = [...this.states, ...this.states2].length - this.trueCount;
   }
 }
